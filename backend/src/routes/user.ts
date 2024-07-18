bb00e6fe0775dde27e0f51cf9b3b9035092a4d19
StrikerCode08 <akashbajpai227@gmail.com>
2024-07-18 13:37:23 +0530

    user login

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,29 +1,49 @@
 import { Router, Request, Response } from "express";
 import { User } from "../models/User";
+import jwt from "jsonwebtoken";
 const router = Router();
 
-router.post('/signup',async (req:Request,res:Response)=>{
-    const { name, userName, password } = req.body;
+router.post("/signup", async (req: Request, res: Response) => {
+  const { name, userName, password } = req.body;
 
+  try {
     const userExists = await User.findOne({ userName });
 
     if (userExists) {
-        res.status(400).json({ message: 'User already exists' });
-        return;
+      res.status(400).json({ message: "User already exists" });
+      return;
     }
 
     const user = new User({ name, userName, password });
 
     if (user) {
-        await user.save();
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            userName: user.userName,
-            token: user.generateToken(user._id),
-        });
+      await user.save();
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        userName: user.userName,
+        token: user.generateToken(user._id),
+      });
     } else {
-        res.status(400).json({ message: 'Invalid user data' });
+      res.status(400).json({ message: "Invalid user data" });
     }
-})
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+router.post("/login", async (req: Request, res: Response) => {
+  const { userName, password } = req.body;
+  try {
+    const user = await User.findOne({ userName });
+    if (!user || !(await user.matchPassword(password))) {
+      throw new Error("Invalid email or password");
+    }
+    const token = jwt.sign({ id: user._id }, `${process.env.JWT_SECRET}`, {
+      expiresIn: "1h",
+    });
+    res.status(200).send({ token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 export default router;
