6f261ce4de5a79515e0a1486e4d8d3cf750adf82
StrikerCode08 <akashbajpai227@gmail.com>
2024-07-18 13:14:04 +0530

    user Signup route

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,20 +1,44 @@
-import mongoose, { Schema, Document } from 'mongoose';
-
+import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
 // Define an interface representing a User document in MongoDB
 interface IUser extends Document {
-    userName: string;
-    password: string;
-    borrowedBooks: mongoose.Types.ObjectId[];
+  userName: string;
+  password: string;
+  name: string;
+  borrowedBooks: mongoose.Types.ObjectId[];
+  matchPassword(candidatePassword: string): Promise<boolean>;
+  generateToken:(id:string)=>string;
+  _id:string;
 }
 
 // Create a Schema corresponding to the document interface
 const UserSchema: Schema = new Schema({
-    userName: { type: String, required: true },
-    password: { type: String, required: true, unique: true },
-    borrowedBooks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }]
+  userName: { type: String, required: true },
+  password: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  borrowedBooks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }],
+});
+// Password hashing middleware
+UserSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
+// Method to compare passwords
+UserSchema.methods.matchPassword = async function (enteredPassword: string) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+//Method to generate JWT
+UserSchema.methods.generateToken = (id: string) => {
+    return jwt.sign({ id }, `${process.env.JWT_SECRECT}`, {
+        expiresIn: `${process.env.JWT_EXPIRY}`,
+    });
+};
+
 // Create a User model
-const User = mongoose.model<IUser>('User', UserSchema);
+const User = mongoose.model<IUser>("User", UserSchema);
 
 export { User, IUser };
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,9 +1,29 @@
 import { Router, Request, Response } from "express";
-
+import { User } from "../models/User";
 const router = Router();
 
-router.get('/signup',(req:Request,res:Response)=>{
+router.post('/signup',async (req:Request,res:Response)=>{
+    const { name, userName, password } = req.body;
 
-    res.json({"message":""})
+    const userExists = await User.findOne({ userName });
+
+    if (userExists) {
+        res.status(400).json({ message: 'User already exists' });
+        return;
+    }
+
+    const user = new User({ name, userName, password });
+
+    if (user) {
+        await user.save();
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            userName: user.userName,
+            token: user.generateToken(user._id),
+        });
+    } else {
+        res.status(400).json({ message: 'Invalid user data' });
+    }
 })
 export default router;
