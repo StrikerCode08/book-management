58cf5e75345c98275689a896685585974b7338a3
StrikerCode08 <akashbajpai227@gmail.com>
2024-07-19 12:15:45 +0530

    tailwind and react router

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,9 @@
+import Link from "react-router-dom"
+
+export default function Login(){
+    return (
+        <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    )
+}
